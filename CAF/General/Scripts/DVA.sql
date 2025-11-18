9b96b91eba5973cef1ac7ed1331136171b24e387
Jérémie <jgadenne@orange.fr>
2025-11-18 14:48:22 +0100

    Ajout de l'année 2023

--- CAF/General/Scripts/DVA.sql
+++ CAF/General/Scripts/DVA.sql
@@ -7,8 +7,24 @@ TRUNCATE TABLE DVA ;
 /* Remplissage de l'année 2024 */
 INSERT INTO DVA 
 SELECT NULL, r.member_id, 3, '2024-12-08', 2, '2024-12-08 21:00:00.000', NULL  FROM program p JOIN caf2_program_registration r ON r.program_id  = p.id 
-WHERE p.id = 15284 -- Sortie Brigitte CORAT
-UNION
+WHERE p.id = 15284
+AND r.status = 1; -- Sortie Brigitte CORAT
+
+INSERT INTO DVA 
 SELECT NULL, r.member_id, 3, '2024-12-15', 2, '2024-12-15 21:00:00.000', NULL  FROM program p JOIN caf2_program_registration r ON r.program_id  = p.id 
-WHERE p.id = 15285; -- Sortie Frédéric FAVIER
+WHERE p.id = 15285
+AND r.status = 1; -- Sortie Frédéric FAVIER
+
+/* Remplissage de l'année 2023 */
+INSERT INTO DVA
+SELECT NULL, r.member_id, 3, '2023-12-10', 2, '2023-12-10 21:00:00.000', NULL  FROM program p JOIN caf2_program_registration r ON r.program_id  = p.id 
+WHERE p.id = 14955 
+AND r.status = 1; -- Sortie Fabien OUILLON
+
+INSERT INTO DVA
+SELECT NULL, r.member_id, 3, '2023-12-17', 2, '2023-12-17 21:00:00.000', NULL  FROM program p JOIN caf2_program_registration r ON r.program_id  = p.id 
+WHERE p.id = 14989
+AND r.status = 1 ; -- Sortie Yann QUILLARD
+
+
 
